6d44d7e40d64d0c6aff3aaebb12892a3817beb09
rootmium <rootmium@gmail.com>
2025-12-14 18:01:21 +0600

    feat(nvim): mason to manage LSP servers, linters and formatters

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,39 @@
+return {
+  "mason-org/mason.nvim",
+  dependencies = { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+  config = function()
+    -- Startup mason
+    require("mason").setup()
+
+    -- Store all packages in one variable
+    local servers = {
+      "clangd",
+      "html-lsp",
+      "css-lsp",
+      "typescript-language-server",
+      "python-lsp-server",
+      "bash-language-server",
+      "jdtls",
+      "lua-language-server",
+      "stylua",
+      "prettierd",
+      "shellcheck",
+      "shfmt",
+      "markdownlint",
+      "mesonlsp",
+      "json-lsp",
+      "eslint_d",
+      "ruff",
+      "docker-compose-language-service",
+      "docker-language-server",
+      -- "nginx-language-server",
+      "nginx-config-formatter",
+      "tailwindcss-language-server",
+      "prisma-language-server",
+    }
+
+    -- Use `mason-tool-installer` package to install everying in 'servers'
+    -- variable
+    require("mason-tool-installer").setup({ ensure_installed = servers })
+  end,
+}
