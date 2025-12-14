334056a46660dce8f08ea971718ee2b22a1c7b26
rootmium <rootmium@gmail.com>
2025-12-14 17:55:20 +0600

    feat(nvim): autocmds for various events

---
+++ .config/nvim/lua/autocmds.lua
@@ -0,0 +1,62 @@
+-- autocmds
+vim.api.nvim_command("autocmd TermOpen * startinsert")
+vim.api.nvim_command("autocmd TermOpen * setlocal nonumber norelativenumber")
+vim.api.nvim_command("autocmd TermEnter * setlocal signcolumn=no")
+
+-- vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+--   callback = function()
+--     vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+--     vim.cmd("hi Comment guifg=grey60 ctermfg=grey")
+--   end,
+-- })
+
+-- vim.api.nvim_create_autocmd(
+--   { "BufEnter", "FocusGained", "InsertLeave", "WinEnter" },
+--   { pattern = "*", command = "if &nu && mode() != 'i' | set rnu | endif" }
+-- )
+--
+-- vim.api.nvim_create_autocmd(
+--   { "BufLeave", "FocusLost", "InsertEnter", "WinLeave" },
+--   { pattern = "*", command = "if &nu | set nornu | endif" }
+-- )
+
+-- Check if it's needed to reload the file when it changes
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = vim.api.nvim_create_augroup("nc-reload-file", { clear = true }),
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
+  end,
+})
+
+-- Go to last location when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("nc-last-loc", { clear = true }),
+  callback = function(event)
+    local buf = event.buf
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("nc-highlight-yank", { clear = true }),
+  callback = function()
+    (vim.hl or vim.highlight).on_yank()
+  end,
+})
+
+-- Resize splits if window got reseized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = vim.api.nvim_create_augroup("nc-resize-split", { clear = true }),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
