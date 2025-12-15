c09574d1bed6eaa249075cf9a4f5df457a81f711
rootmium <rootmium@gmail.com>
2025-12-15 15:20:42 +0600

    refactor(nvim): remove keybind for git blame

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,7 +1,4 @@
 return {
-  keys = {
-    { "<leader>gb", ":Gitsigns toggle_current_line_blame<cr>", desc = "Show git blame (current line)" },
-  },
   "lewis6991/gitsigns.nvim",
   opts = {
     current_line_blame = true,
