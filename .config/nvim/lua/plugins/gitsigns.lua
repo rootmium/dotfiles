a983959f3c2151166f97e5baf0903032a4500672
rootmium <rootmium@gmail.com>
2025-12-15 15:22:55 +0600

    feat(nvim): show git info on the line number instead of the signcolumn

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -3,5 +3,11 @@ return {
   lazy = false,
   opts = {
     current_line_blame = true,
+    signcolumn = false,
+    numhl = true,
+    current_line_blame_opts = {
+      virt_text_pos = "right_align",
+      delay = 10,
+    },
   },
 }
